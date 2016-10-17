fec50bf1331ecaebf0c6f4023bd63b2b3304476e
Iber <iberparodisiri@gmail.com>
2016-10-17 00:42:52 -0300

    Fix indentation problems

--- src/com/rombus/evilbones/lmg/SessionListener.java
+++ src/com/rombus/evilbones/lmg/SessionListener.java
@@ -1,49 +1,52 @@
 package com.rombus.evilbones.lmg;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
 /**
- * Thread that listens to melted responses and prints 
- * them through a SessionNotifier interface.
- * 
+ * Thread that listens to melted responses and prints them through a SessionNotifier interface.
+ *
  * @author rombus
  *
  * 23/07/2016 12:18:21
  */
 public class SessionListener implements Runnable {
-	private boolean keepListening;
-	private I_SessionNotifier notifier;
-	private BufferedReader socketIn;
-	
-	public SessionListener(BufferedReader socketIn, I_SessionNotifier notifier){
-		this.keepListening = true;
-		this.socketIn = socketIn;
-		this.notifier = notifier;
-	}
-	
-	public void stopListening() throws IOException{
-		this.keepListening = false;
-	}
-	
-	@Override
-	public void run() {
-		while(keepListening){
-        	try {
-        		String result = socketIn.readLine();        		
-        		if(result != null){
-        			notifier.writeOutput(result);
-        		}
-			} catch (IOException e1) {}
-        	
-        	try { 
-        		Thread.sleep(5); 
-        	} catch (InterruptedException e) { 
-        		return;
-        	}
+
+    private boolean keepListening;
+    private I_SessionNotifier notifier;
+    private BufferedReader socketIn;
+
+    public SessionListener(BufferedReader socketIn, I_SessionNotifier notifier) {
+        this.keepListening = true;
+        this.socketIn = socketIn;
+        this.notifier = notifier;
+    }
+
+    public void stopListening() throws IOException {
+        this.keepListening = false;
+    }
+
+    @Override
+    public void run() {
+        while (keepListening) {
+            try {
+                String result = socketIn.readLine();
+                if (result != null) {
+                    notifier.writeOutput(result);
+                }
+            } catch (IOException e1) {
+            }
+
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                return;
+            }
         }
-		
-		try {
-			socketIn.close();
-		} catch (IOException e) { }
-	}
+
+        try {
+            socketIn.close();
+        } catch (IOException e) {
+        }
+    }
 }
--- src/com/rombus/evilbones/lmg/SessionNotifier.java
+++ src/com/rombus/evilbones/lmg/SessionNotifier.java
@@ -8,109 +8,103 @@ import javafx.scene.control.TextArea;
  *
  * 20/08/2016 20:51:59
  */
-public class SessionNotifier implements I_SessionNotifier{
-	private TextArea outputText;
-	private I_SessionNotifier.Commands executedCommand;
-	private int msgCounter; // Helper para interpretar respuestas que tienen más de un mensaje
-	
-	public SessionNotifier(TextArea outputText){
-		this.outputText = outputText;
-		this.executedCommand = Commands.NO_CMD;
-		this.msgCounter = 0;
-	}
+public class SessionNotifier implements I_SessionNotifier {
 
-	@Override
-	public void writeOutput(final String msg) {
-		Platform.runLater(new Runnable() {
-			@Override
-			public void run() {
-				// Si es un mensaje hacia melted, imprimo y salgo.
-				if(msg.contains(">>")){
-					outputText.appendText(msg+"\n");
-					return;
-				}
-				
-				
-				switch(executedCommand){
-				case USTA:
-					// Muestro el mensaje de OK
-					if(msg.contains(" OK")){
-						outputText.appendText(msg+"\n");
-					}
-					// Proceso la respuesta del USTA
-					else {
-						String msgs[] = msg.split(" ");
-						
-						if(msgs.length == 17) {	//Valido que haya sido OK la respuesta, si no tiene todos estos campos fue fail
-							outputText.appendText("\n");
-							//outputText.appendText("UNIT: "+msgs[0]+"\n"); // No lo muestro pq ya se que es la U0
-							outputText.appendText("  mode:\t\t\t"+msgs[1]+"\n");
-							outputText.appendText("  path:\t\t\t"+msgs[2]+"\n");
-							outputText.appendText("  cur frame:\t\t"+msgs[3]+"\n");
-							outputText.appendText("  speed %:\t\t"+msgs[4]+"\n");
-							outputText.appendText("  fps:\t\t\t"+msgs[5]+"\n");
-							outputText.appendText("  in-point:\t\t"+msgs[6]+"\n");
-							outputText.appendText("  out-point:\t\t"+msgs[7]+"\n");
-							outputText.appendText("  length:\t\t"+msgs[8]+"\n");
-							outputText.appendText("  tail clip name:\t"+msgs[9]+"\n");
-							outputText.appendText("  tail pos:\t\t"+msgs[10]+"\n");
-							outputText.appendText("  tail in-point:\t"+msgs[11]+"\n");
-							outputText.appendText("  tail out-point:\t"+msgs[12]+"\n");
-							outputText.appendText("  tail length:\t\t"+msgs[13]+"\n");
-							outputText.appendText("  seekable flag:\t"+msgs[14]+"\n");
-							outputText.appendText("  playlist num:\t\t"+msgs[15]+"\n");
-							outputText.appendText("  clip index:\t\t"+msgs[16]+"\n");
-							outputText.appendText("\n");
-						}
-						else{
-							System.out.println("else");
-							outputText.appendText(msg+"\n");		
-						}
-					}
-					break;
-				
-				case LIST:
-					// Muestro el mensaje de OK
-					if(msg.contains(" OK")){
-						outputText.appendText(msg+"\n");
-						msgCounter = 0;
-					}
-					// Proceso la respuesta del USTA
-					else if (msgCounter == 0){
-						msgCounter++;
-						outputText.appendText("\n");
-						outputText.appendText("Modificaciones a la playlist: "+msg+"\n\n");
-					}
-					else {
-						msgCounter++;	// Son multiples mensajes que tengo que procesar
-						String msgs[] = msg.split(" ");
-						
-						if(msgs.length == 7) {	//Valido que haya sido OK la respuesta, si no tiene todos estos campos fue fail
-							outputText.appendText("--------------------\n");
-							outputText.appendText("  clip index:\t\t"+msgs[0]+"\n"); // No lo muestro pq ya se que es la U0
-							outputText.appendText("  path:\t\t\t"+msgs[1]+"\n");
-							outputText.appendText("  in-point:\t\t"+msgs[2]+"\n");
-							outputText.appendText("  out-point:\t\t"+msgs[3]+"\n");
-							outputText.appendText("  length:\t\t"+msgs[4]+"\n");
-							outputText.appendText("  calculated len:\t"+msgs[5]+"\n");
-							outputText.appendText("  fps:\t\t\t"+msgs[6]+"\n");
-						}
-						else{
-							outputText.appendText(msg+"\n");		
-						}
-					}
-					break;
-					
-				default:
-					outputText.appendText(msg+"\n");
-				}	
-			}
-		});		
-	}
+    private TextArea outputText;
+    private I_SessionNotifier.Commands executedCommand;
+    private int msgCounter; // Helper para interpretar respuestas que tienen más de un mensaje
 
-	
-	@Override
-	public void setSentMsg(Commands cmd) {
-		this.executedCommand = cmd;
-	}
+    public SessionNotifier(TextArea outputText) {
+        this.outputText = outputText;
+        this.executedCommand = Commands.NO_CMD;
+        this.msgCounter = 0;
+    }
+
+    @Override
+    public void writeOutput(final String msg) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                // Si es un mensaje hacia melted, imprimo y salgo.
+                if (msg.contains(">>")) {
+                    outputText.appendText(msg + "\n");
+                    return;
+                }
+
+                switch (executedCommand) {
+                    case USTA:
+                        // Muestro el mensaje de OK
+                        if (msg.contains(" OK")) {
+                            outputText.appendText(msg + "\n");
+                        } // Proceso la respuesta del USTA
+                        else {
+                            String msgs[] = msg.split(" ");
+
+                            if (msgs.length == 17) {	//Valido que haya sido OK la respuesta, si no tiene todos estos campos fue fail
+                                outputText.appendText("\n");
+                                //outputText.appendText("UNIT: "+msgs[0]+"\n"); // No lo muestro pq ya se que es la U0
+                                outputText.appendText("  mode:\t\t\t" + msgs[1] + "\n");
+                                outputText.appendText("  path:\t\t\t" + msgs[2] + "\n");
+                                outputText.appendText("  cur frame:\t\t" + msgs[3] + "\n");
+                                outputText.appendText("  speed %:\t\t" + msgs[4] + "\n");
+                                outputText.appendText("  fps:\t\t\t" + msgs[5] + "\n");
+                                outputText.appendText("  in-point:\t\t" + msgs[6] + "\n");
+                                outputText.appendText("  out-point:\t\t" + msgs[7] + "\n");
+                                outputText.appendText("  length:\t\t" + msgs[8] + "\n");
+                                outputText.appendText("  tail clip name:\t" + msgs[9] + "\n");
+                                outputText.appendText("  tail pos:\t\t" + msgs[10] + "\n");
+                                outputText.appendText("  tail in-point:\t" + msgs[11] + "\n");
+                                outputText.appendText("  tail out-point:\t" + msgs[12] + "\n");
+                                outputText.appendText("  tail length:\t\t" + msgs[13] + "\n");
+                                outputText.appendText("  seekable flag:\t" + msgs[14] + "\n");
+                                outputText.appendText("  playlist num:\t\t" + msgs[15] + "\n");
+                                outputText.appendText("  clip index:\t\t" + msgs[16] + "\n");
+                                outputText.appendText("\n");
+                            } else {
+
+                                outputText.appendText(msg + "\n");
+                            }
+                        }
+                        break;
+
+                    case LIST:
+                        // Muestro el mensaje de OK
+                        if (msg.contains(" OK")) {
+                            outputText.appendText(msg + "\n");
+                            msgCounter = 0;
+                        } // Proceso la respuesta del USTA
+                        else if (msgCounter == 0) {
+                            msgCounter++;
+                            outputText.appendText("\n");
+                            outputText.appendText("Modificaciones a la playlist: " + msg + "\n\n");
+                        } else {
+                            msgCounter++;	// Son multiples mensajes que tengo que procesar
+                            String msgs[] = msg.split(" ");
+
+                            if (msgs.length == 7) {	//Valido que haya sido OK la respuesta, si no tiene todos estos campos fue fail
+                                outputText.appendText("--------------------\n");
+                                outputText.appendText("  clip index:\t\t" + msgs[0] + "\n"); // No lo muestro pq ya se que es la U0
+                                outputText.appendText("  path:\t\t\t" + msgs[1] + "\n");
+                                outputText.appendText("  in-point:\t\t" + msgs[2] + "\n");
+                                outputText.appendText("  out-point:\t\t" + msgs[3] + "\n");
+                                outputText.appendText("  length:\t\t" + msgs[4] + "\n");
+                                outputText.appendText("  calculated len:\t" + msgs[5] + "\n");
+                                outputText.appendText("  fps:\t\t\t" + msgs[6] + "\n");
+                            } else {
+                                outputText.appendText(msg + "\n");
+                            }
+                        }
+                        break;
+
+                    default:
+                        outputText.appendText(msg + "\n");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void setSentMsg(Commands cmd) {
+        this.executedCommand = cmd;
+    }
 }
